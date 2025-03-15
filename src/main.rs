d8df36eb790a405f0e63bfdfa0e4bd1eb4674ae1
LucasWillBlumenau <willlucas07@proton.me>
2025-03-15 16:20:50 -0300

    adding exit command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
     let stdin = io::stdin();
@@ -10,8 +11,40 @@ fn main() {
         
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-    
-        println!("{}: command not found", input.trim())
+
+        if input.starts_with("exit ") {
+            
+            let code = get_string_content_after_text(&input, "exit ").trim();
+            let result = code.parse::<i32>();
+
+            match result {
+                Ok(code) => {
+                    exit(code);
+                }
+                Err(_) => {
+                    println!("invalid argument for exit command exit: {}", code)
+                }
+            }
+        } else {
+            println!("{}: command not found", input.trim())
+        }
+        
     }
+
+}
+
+fn get_string_content_after_text<'a>(text: &'a str, subtext: &str) -> &'a str {
+
+    let subtext_length = subtext.len();
+
+    for i in 0..text.len() {
+
+        if &text[i..subtext_length + i] == subtext {
+            return &text[i + subtext_length..]
+        }
+
+    }
+
+    return ""
 
 }
