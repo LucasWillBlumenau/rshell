d56bf9d733939ed746de6309efdd64bf3419e5cf
LucasWillBlumenau <willlucas07@proton.me>
2025-03-16 17:55:24 -0300

    feat: adding '~' home directory alias

--- src/commands/cd.rs
+++ src/commands/cd.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{env::VarError, path::Path};
 
 pub fn cd(args: &str) -> () {
     let args: Vec<&str> = args.trim()
@@ -11,15 +11,36 @@ pub fn cd(args: &str) -> () {
         return;
     }
 
-    let dir = args[0];
-    let path = Path::new(dir);
-    if !path.is_dir() {
-        println!("cd: {}: No such file or directory", dir);
-        return;
+    let dir = expand_user_path(args[0]);
+    match dir {
+        Ok(dir) => {
+            let path = Path::new(&dir);
+            if !path.is_dir() {
+                println!("cd: {}: No such file or directory", dir);
+                return;
+            }
+        
+            let result = std::env::set_current_dir(path);
+            if let Err(err) = result {
+                println!("{}", err);
+            }
+        }
+        Err(err) => {
+            println!("{}", err);
+        }
+    } 
+ 
+}
+
+
+fn expand_user_path(path: &str) -> Result<String, VarError> {
+    if !path.starts_with("~") {
+        return Ok(path.to_owned());
     }
 
-    let result = std::env::set_current_dir(path);
-    if let Err(err) = result {
-        println!("{}", err);
+    let home_dir = std::env::var("HOME");
+    match home_dir {
+        Ok(home_dir) => Ok(path.replacen("~", &home_dir, 1)),
+        Err(err) => Err(err)
     }
 }
