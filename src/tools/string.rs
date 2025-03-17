2114bca4f5bb7b76e697f466cceb06a5d5d567bb
LucasWillBlumenau <willlucas07@proton.me>
2025-03-17 20:37:35 -0300

    fix: fixing args parsing

--- src/tools/string.rs
+++ src/tools/string.rs
@@ -15,46 +15,68 @@ pub fn split(text: &str, sep: char) -> (&str, &str) {
 
 pub fn split_args(text: &str) -> Result<Vec<String>, Error> {
 
-    let text = text.trim_end()
-        .trim_start();  
+    let mut text = text.trim_end()
+        .trim_start();
 
-    let mut quote = '\0';
     let mut args: Vec<String> = vec![];
-    let mut escaped = false;
 
-    let mut buffer = String::new();
-
-    for c in text.chars() {
-
-        if c == quote && !escaped {
-            args.push(buffer.clone());
-            buffer.clear();
-            quote = '\0';
-        } else if c == '\'' && quote == '\0'  {
-            quote = '\'';
-        } else if c == '"' && quote == '\0' {
-            quote = '"';
-        } else if c == ' ' && quote == '\0' {
-            if buffer != "" {
-                args.push(buffer.clone());
-                buffer.clear();
-            }
-        } else if c == '\\'{
-            escaped = true;
-        }    
-        else {
-            buffer.push(c);
-            escaped = false;
+    while text.len() > 0 {
+        let index: usize;
+        let arg: String;
+        if text.starts_with('\'') {
+            (index, arg) = search_until_close_quote(&text, '\'')?;
+        } else if text.starts_with('"') {
+            (index, arg) = search_until_close_quote(&text, '"')?;
+        } else {
+            (index, arg) = search_until_whitespace(&text);
         }
 
+        args.push(arg);
+        text = &text[index..text.len()].trim_start();
     }
 
-    if quote != '\0' {
-        return Err(Error::msg(format!("quote {} not closed", quote)));
-    }
-
-    if buffer != "" {
-        args.push(buffer);
-    }
     Ok(args)
+}
+
+
+fn search_until_close_quote(text: &str, quote: char) -> Result<(usize, String), Error> {
+    let mut close_buffer = false;
+    let mut buffer = String::new();
+    for (i, c) in text.chars().enumerate() {
+        if i == 0 {
+            continue;
+        }
+        
+        if close_buffer {
+            if c != quote {
+                return Ok((i + 1, buffer));
+            }
+            close_buffer = false;
+        }
+        else if c != quote {
+            buffer.push(c);
+        } else {
+            close_buffer = true;
+        }
+    }
+
+    if close_buffer {
+        Ok((text.len(), buffer))
+    } else {
+        Err(Error::msg(format!("{} not closed", quote)))
+    }
+
+}
+
+
+fn search_until_whitespace(text: &str) -> (usize, String) {
+    let mut buffer = String::new();
+    for (i, c) in text.chars().enumerate() {
+        if c == ' ' {
+            return (i + 1, buffer);
+        }
+        buffer.push(c);
+    }
+
+    return (text.len(), buffer);
 }
